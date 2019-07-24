42510a737a9fb4cf0d46648932d3b670c46fc4ab
Chris <Silber8806@gmail.com>
2019-07-24 19:29:48 -0400

    finishing sign-up form

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
-# accounts/views.py
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'accounts/signup.html'
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
