fe07e064d349322920fe4e0ec6a18c62f19a2a25
Chris <Silber8806@gmail.com>
2019-07-26 09:27:04 -0400

    updating

--- orders/models.py
+++ orders/models.py
@@ -39,7 +39,7 @@ class ProductGroupOption(models.Model):
 
 
 class ProductCategory(models.Model):
-    product_category_name = models.CharField(max_length=64, blank=True)
+    product_category_name = models.CharField(max_length=64)
     sort_order = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
@@ -47,30 +47,30 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=64, blank=True)
-    product_category_fk = models.ForeignKey(ProductCategory, on_delete=models.DO_NOTHING, null=True, blank=True)
-    product_group_fk = models.ForeignKey(ProductGroup, on_delete=models.DO_NOTHING, null=True, blank=True)
+    product_name = models.CharField(max_length=64)
+    product_category_fk = models.ForeignKey(ProductCategory, on_delete=models.DO_NOTHING)
+    product_group_fk = models.ForeignKey(ProductGroup, on_delete=models.DO_NOTHING)
     product_unit_price = models.DecimalField(max_digits=5, decimal_places=2)
-    option_num_min = models.IntegerField(blank=True, null=True)
-    option_num_max = models.IntegerField(blank=True, null=True)
+    option_num_min = models.IntegerField(default=0)
+    option_num_max = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.product_name} - { self.product_category_fk.product_category_name}- $ {self.product_unit_price}"
 
 
 class ProductVariation(models.Model):
-    variation_name = models.CharField(max_length=64,null=True, blank=True)
-    variation_category = models.CharField(max_length=64,null=True, blank=True)
-    variation_unit_price = models.DecimalField(max_digits=5, decimal_places=2,null=True, blank=True)
-    product_fk = models.ForeignKey(Product, on_delete=models.CASCADE,null=True, blank=True)
+    variation_name = models.CharField(max_length=64)
+    variation_category = models.CharField(max_length=64)
+    variation_unit_price = models.DecimalField(max_digits=5, decimal_places=2)
+    product_fk = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.product_fk.product_name} - {self.variation_name} - $ {self.variation_unit_price + self.product_fk.product_unit_price}"
 
 
 class Order(models.Model):
-    customer_fk = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="customer")
-    address_fk = models.ForeignKey(Address, on_delete=models.DO_NOTHING, related_name="address")
+    customer_fk = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    address_fk = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
     order_status = models.CharField(max_length=64)
     gross_amt = models.DecimalField(max_digits=12, decimal_places=2)
     tax_amt = models.DecimalField(max_digits=12, decimal_places=2)
@@ -82,8 +82,8 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order_fk = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order")
-    product_fk = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name="product")
+    order_fk = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_fk = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     unit_price = models.DecimalField(max_digits=5, decimal_places=2)
     quantity = models.IntegerField()
 
